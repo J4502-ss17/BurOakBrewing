a7de6395d0f983330d3e67e2493d746ab3ad0a06
brianasaunders <bnsz9f@mail.missouri.edu>
2017-04-17 15:48:49 -0500

    Photo Gallery
    Created it but now need to figure out style

--- buroak.js
+++ buroak.js
@@ -12,3 +12,23 @@ $(document).ready(function() {
 	 }
 	});//end resize
 });//end ready
+
+
+//test for photo gallery
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("gallery");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
